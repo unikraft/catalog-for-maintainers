b01d5df380e4a452e1b92d63f795fbfe9e13dd6d
Razvan Deaconescu <razvand@unikraft.io>
2023-11-28 18:03:02 +0200

    feat(examples): Introduce Python 3 helloworld
    Introduce Python3 as binary compatibility run printing "Hello, World!".
    Extract / Install Python using `Dockerfile`. Then run it with the `base`
    kernel images from `../../kernels/`.
    
    Add typical files for a bincompat app:
    
    * `Kraftfile`: build / run rules, including pulling the `base` image
    * `Dockerfile`: filesystem, including binary and libraries
    * `Makefile`: used to generate the root filesystem from the `Dockerfile`
      rules
    * `README.md`: instructions to set up, build and run the application
    * `config.yaml`: configuration file to generate scripts to the
      application
    * `helloworld.py`: Python script to print "Hello, World!"
    
    `config.yaml` is used to generate run scripts using the
    `../../utils/bincompat/generate.py` script.
    
    The kernels in `../../kernels` are generated by running the
    `../../utils/bincompat/base-build-all.sh` script while inside the
    `../../library/base/` directory.
    
    Signed-off-by: Razvan Deaconescu <razvand@unikraft.io>

---
+++ examples/hello-python3/helloworld.py
@@ -0,0 +1,14 @@
+"""
+Simple Hello, World! printing program
+"""
+
+import sys
+
+
+def main():
+    """Print Hello, World!"""
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    sys.exit(main())
