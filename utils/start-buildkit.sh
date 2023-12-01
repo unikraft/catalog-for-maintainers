abcfba37d3fae5f1c50ce376e886d90f1915146d
Razvan Deaconescu <razvand@unikraft.io>
2023-12-01 09:12:14 +0200

    fix(utils): Start BuildKit container if it was stopped
    Currently the `utils/start-buildkitd.sh` was starting the BuildKit
    container if the container didn't exist. It didn't start it in case it
    was stopped.
    
    This commit fixes this, by looking if the container exists and then
    starting it.
    
    Signed-off-by: Razvan Deaconescu <razvand@unikraft.io>

--- utils/start-buildkit.sh
+++ utils/start-buildkit.sh
@@ -2,3 +2,4 @@
 
 export KRAFTKIT_BUILDKIT_HOST=docker-container://buildkitd
 docker container inspect buildkitd > /dev/null 2>&1 || docker run -d --name buildkitd --privileged moby/buildkit:latest
+test $(docker container inspect -f '{{.State.Running}}' buildkitd 2> /dev/null) = "true" || docker start buildkitd
