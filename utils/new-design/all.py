9037b2a92350ba21ceff28d938bdb5ca5b3b7b65
Razvan Deaconescu <razvand@unikraft.io>
2024-07-19 21:50:33 +0300

    Document new class design
    Signed-off-by: Razvan Deaconescu <razvand@unikraft.io>

--- utils/new-design/all.py
+++ utils/new-design/all.py
@@ -2,19 +2,39 @@ import os
 import yaml
 import itertools
 
-class ConfigVariants:
-    arch = ['arm64', 'x86_64']
-    hypervisor = ['none', 'kvm', 'xen']
-    platform = ['qemu', 'firecracker', 'xen']
-    build_tool = ['make', 'kraft']
-    run_tool = ['vmm', 'kraft']
-    bootloader = ['multiboot', 'uefi']
-    pie = [False, True]
-    debug = ['none', 'strace', 'err', 'info', 'debug']
-    fs = ['none', 'initrd', '9pfs']
-    networking = ['none', 'bridge', 'nat', 'tap']
+class TesterConfig:
+    """Store tester configuration.
 
-    def _convert_to_dict(i):
+    Configuration is read from the global tester configuration file and the
+    application-specific configuration file.
+    """
+
+    def __init__(self, config_file, app_config_file=None):
+        try:
+            with open(config_file, "r", encoding="utf8") as stream:
+                self.config = yaml.safe_load(stream)
+        except IOError:
+            print(f"Error: Unable to open configuration file '{config_file}'", file=sys.stderr)
+            return None
+
+        if not app_config_file:
+            if os.path.isfile("tester.yaml"):
+                app_config_file = "tester.yaml"
+
+        app_config = None
+        if app_config_file:
+            try:
+                with open(config_file, "r", encoding="utf8") as stream:
+                    app_config = yaml.safe_load(stream)
+            except IOError:
+                print(f"Error: Unable to open configuration file '{config_file}'", file=sys.stderr)
+                return None
+
+        print(self.config)
+        if app_config:
+            print(app_config)
+
+    def _convert_to_build_dict(i):
         return {
                 'arch': i[0],
                 'hypervisor': i[1],
@@ -27,33 +47,24 @@ class ConfigVariants:
                 'networking': i[8]
                 }
 
-    def _is_valid_config(d):
-        if d['networking'] == 'bridge' and d['platform'] == 'firecracker':
-            return False
-        if d['networking'] == 'tap' and d['platform'] != 'firecracker':
-            return False
-        if d['bootloader'] == 'uefi' and d['arch'] != 'x86_64':
-            return False
-        if d['platform'] == 'firecracker' and d['hypervisor'] != 'kvm':
-            return False
-        if d['platform'] == 'xen' and d['hypervisor'] != 'xen':
-            return False
-        if d['platform'] == 'qemu' and (d['hypervisor'] != 'kvm' and d['hypervisor'] != 'none'):
-            return False
-        if d['fs'] == '9pfs' and d['platform'] == 'firecracker':
-            return False
-        return True
-
-    def generate_config_variants(self):
+    def generate_build_config_variants(self):
         variants = []
-        for i in itertools.product(self.arch, self.hypervisor, self.platform,
+        for i in itertools.product(self.config['variants']['arch'],
+                                   self.config['variants']['hypervisor'],
+                                   self.config['variants']['platform'],
+                                   self.config['variants']['build_tool'],
+                                   self.config['variants']['run_tool'],
+                                   self.config['variants']['bootloader'],
+                                   self.config['variants']['pie'],
+                                   self.hypervisor, self.platform,
                                    self.build_tool, self.run_tool,
                                    self.bootloader, self.debug, self.fs,
                                    self.networking):
-            d = ConfigVariants._convert_to_dict(i)
+            d = ConfigVariants._convert_to_build_dict(i)
             if ConfigVariants._is_valid_config(d):
                 variants.append(d)
         return variants
+
 
 class SystemConfig:
     OS_TYPE_NONE = None
@@ -120,7 +131,7 @@ class AppConfig:
             }
     libs = []
     targets = []
-    scripts_dir = os.path.join(os.getcwd(), "scripts")
+    tester_dir = os.path.join(os.getcwd(), "tester")
     sources = {
             'type': SOURCES_TYPE_NONE,
             'path': None
@@ -141,6 +152,7 @@ class AppConfig:
         for t in self.targets:
             pass
 
+
 class TargetConfig:
     PLATFORM_NONE = None
     ARCH_NONE = None
@@ -158,6 +170,14 @@ class TargetConfig:
     def __init__(self):
         pass
 
+    def generate(self):
+        # Create directory.
+        # Generate config.yaml.
+        self.build_config.generate()
+        self.run_config.generate()
+        pass
+
+
 class BuildConfig:
     BUILD_TOOL_NONE = None
     build_tool = {
@@ -170,6 +190,12 @@ class BuildConfig:
 
     def __init__(self):
         pass
+
+    def generate(self):
+        # Generate build.sh script.
+        # Optionally, generate defconfig file.
+        pass
+
 
 class RunConfig:
     NET_TYPE_NONE = None
@@ -186,9 +212,19 @@ class RunConfig:
     def __init__(self):
         pass
 
+    def generate(self):
+        # Generate run.sh script.
+        pass
+
+
 class TestRunner:
     pass
 
-c = ConfigVariants()
-for i in c.generate_config_variants():
-    print(i)
+
+t = TesterConfig("tester.yaml")
+a = AppConfig()
+s = SystemConfig()
+
+targets = generate_target_configs(t, a, s)
+for t in targets:
+    t.generate()
